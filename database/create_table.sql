21f9b1dfbdc3f9479687697301ebbe8086a0759b
Demerson Rodrigues <demersona.rodrigues@gmail.com>
2023-08-30 00:02:58 -0300

    add screen Myshops (form) and table sales

--- database/create_table.sql
+++ database/create_table.sql
@@ -5,3 +5,13 @@ CREATE TABLE products (
     price DECIMAL(10, 2) NOT NULL,
     category VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE sales (
+    id_sale INT PRIMARY KEY,
+    id_product INT,
+    qtd_items INTEGER,
+    created_at TIMESTAMP,
+    price_product DECIMAL(10, 2),
+    price_discount DECIMAL(10, 2),
+    FOREIGN KEY (id_product) REFERENCES products(id)
+);
